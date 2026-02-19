281b0ced146994264e7a56f9024f950cbf4f37cb
unicornfarm5 <linea.moltved@gmail.com>
2026-02-19 15:16:56 +0100

    small changes

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ I systemdesign
 
 kør test:
 python -m unittest
+
+
+# Præsentation
+Værktøj: pythons eksisterende bibliotek unittest
+    ulempe: vi skulel lave en funktion i klassen så der var flere navne. Måske lidt svær at sætte op
+    
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import math
 
+# testet med test.py
 def multiplication(x):
     return x * x
 
 
+# testes med test2.py
 def makeUpperCase(string):
     return string.upper()
 
 
-if __name__ == '__main__':    print(multiplication(2))
+if __name__ == '__main__':
